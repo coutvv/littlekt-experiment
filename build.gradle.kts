28b197c5d65950441a5d362ce95814a52a6241b5
Roman Lomovtsev <coutvv@gmail.com>
2023-07-15 20:17:32 +0300

    init

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.9.0"
+    application
+}
+
+group = "com.lomovtsev"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+
+val littleKtVersion = "0.7.0" // get the latest release at the top
+val kotlinCoroutinesVersion = "1.6.4" // or whatever version you are using
+
+dependencies {
+    testImplementation(kotlin("test"))
+
+    implementation("com.lehaine.littlekt:core:$littleKtVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinCoroutinesVersion")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "17"
+}
+
+application {
+    mainClass.set("MainKt")
+}
